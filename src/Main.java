51582e6a9a0108d9bd5fcee8c3f9e133cac08e83
olensis <olensis7gmail@.com>
2022-09-03 12:45:06 +0300

    выполнила 2 задания, остальные позже

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,25 @@
 public class Main {
     public static void main(String[] args) {
 
-        var Homework5= 5;
+        var Homework5 = 5;
         var task = 1;
+        int clientOS = 0;
+        if (clientOS == 0)
+            System.out.println("«Установите версию приложения для iOS по ссылке»");
+    else if (clientOS != 0)
+            System.out.println("Установите версию приложения для Android по ссылке");
 
+        var task2 = 2;
+        int clientOS1 = 0;
+        int year =2014;
+        if (clientOS1 == 0)
+            System.out.println("«Установите приложения для iOS по ссылке");
+        else if (clientOS1 != 0 )
+            System.out.println("Установите  приложения для Android по ссылке");
+        if (clientOS1 == 0 && year < 2015)
+            System.out.println( "Установите облегченную версию приложения для iOS по ссылке");
+        else if (clientOS1 != 0 && year < 2015)
+            System.out.println( "Установите облегченную версию приложения для Android по ссылке");
 
 
     }
